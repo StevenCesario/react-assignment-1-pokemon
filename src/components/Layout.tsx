d52dcfd381c2def022c0d3f1255b2143197543e7
StevenCesario <steven.cesario@proton.me>
2026-05-04 15:50:31 +0200

    layout wired up and working! 🌱

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
 
 const Layout = () => {
   return (
-    <div>Layout</div>
+    <div>
+      {/* This is our nav that will be visible and clickable on *EVERY* page */}
+      <nav>
+        <Link to="/">Search Cards</Link>
+        <Link to="/collection">My Collection</Link>
+        <Link to="/create">Create Custom Card</Link>
+      </nav>
+
+      <main>
+        {/* The child page that we are currently looking at (Search, Collection, or Create) will get injected here 🌱 */}
+        <Outlet />
+      </main>
+    </div>
   )
 }
 
-export default Layout
+export default Layout;
