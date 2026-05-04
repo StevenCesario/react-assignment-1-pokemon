3c782cf02a66c533c2ecade52e3b67a27f9cbbe4
StevenCesario <steven.cesario@proton.me>
2026-05-04 16:07:38 +0200

    start of a collection page + reflections on search + layout.jsx, not layout.tsx

--- src/pages/CollectionPage.jsx
+++ src/pages/CollectionPage.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { useState } from 'react'
 
 const CollectionPage = () => {
+  const [cards, setCards] = useState([]); // Will need to change so that we check a JSON or something on mount
+
   return (
-    <div>CollectionPage</div>
+    <div>
+      {cards.length === 0 ? (
+        <p>There are no cards in your collection! Add your first card now :)</p>
+      ) : (
+        cards.map(card => <Card card={card} />)
+      )}
+    </div>
   )
 }
 
--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -3,6 +3,9 @@ import { useState, useEffect } from 'react'
 const SearchPage = () => {
   const [searchTerm, setSearchTerm] = useState('');
 
+  // The main thing I'm gonna have to think about here is... 
+  // 1. On the Home/Main page I want debounced search. You should be able to search.. from any page. I want a search bar in the nav! Hmmmm. For next focus block
+
   useEffect(() => {
     // Will do... something haha!
   }, [searchTerm]);
