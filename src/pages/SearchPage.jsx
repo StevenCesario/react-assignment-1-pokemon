b6387d95be26dc74909b823f7cd1729420cd0385
StevenCesario <steven.cesario@proton.me>
2026-05-04 15:58:57 +0200

    starts of a search page

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 
 const SearchPage = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    // Will do... something haha!
+  }, [searchTerm]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    // To be implemented
+  }
+
   return (
-    <div>SearchPage</div>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type='text' onChange={(e) => setSearchTerm(e.target.value)} />
+        <button type='submit'>Search</button>
+      </form>
+      <p>Search term is currently {searchTerm}</p>
+    </div>
   )
 }
 
