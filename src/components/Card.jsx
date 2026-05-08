fe797aa391ac2194e69a88da4eeb97cde51363fb
StevenCesario <steven.cesario@proton.me>
2026-05-08 14:32:15 +0200

    optional chaining applied to card data in Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -70,8 +70,9 @@ const Card = ({ card }) => {
     // "<li> for now"; now a re-usable <div> that is used in the SearchResult component - *there* we use <li>!
     <div>
       {/* Let's just start with this */}
-      <h2>{card.card_info.name}</h2>
-      <h3>Set: {card.card_info.set_name}</h3>
+      {/* Ended up only using these haha! Now also with optional chaining just to be absolute bulletproof */}
+      <h2>{card.card_info?.name}</h2>
+      <h3>Set: {card.card_info?.set_name}</h3>
 
       {/* Just noticing now that this endpoint... doesn't have images. That's a bummer. Let's check the API docs. */}
       {/* Right. Images have their own endpoint: GET /images/:id with the id under the 'id' key in a results object */}
