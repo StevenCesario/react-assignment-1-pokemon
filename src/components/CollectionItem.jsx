f4e1c9882115214a0d7656d43ecd4fd90c15fc8f
StevenCesario <steven.cesario@proton.me>
2026-05-08 13:08:42 +0200

    images in collection are now clickable! 🚀

--- src/components/CollectionItem.jsx
+++ src/components/CollectionItem.jsx
@@ -1,11 +1,13 @@
 // import Card from "./Card" Not used anymore
+import { Link } from "react-router-dom"
 import CardImage from "./CardImage"
 
 const CollectionItem = ({ card, onIncrease, onDecrease, onDelete }) => {
   return (
     <div>
       {/* Now only includes the image! No Card component for the Name or Set! */}
-      <CardImage cardId={card.id} cardName={card.card_info.name} />
+      {/* Now also clickable! 🚀 */}
+      <Link to={`/card/${card.id}`} state={{ cardData: card }} ><CardImage cardId={card.id} cardName={card.card_info.name} /></Link>
       {/* The onClicks need to be arrow functions since we're passing arguments to keep them as function references! 🚀 */}
       <button disabled={card.amount === 1} onClick={() => onDecrease(card.id)}>-</button>
       <span>Amount: {card.amount}</span>
