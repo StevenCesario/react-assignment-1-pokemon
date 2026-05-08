fef5712860d45682e053772730f3eb7e836ea21e
StevenCesario <steven.cesario@proton.me>
2026-05-08 08:41:28 +0200

    lifting state up! user collection state array now lives in App.jsx, not CollectionPage.jsx!

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Layout from './components/Layout';
 import CollectionPage from './pages/CollectionPage';
@@ -7,16 +8,21 @@ import DetailedViewPage from './pages/DetailedViewPage';
 import CreatePage from './pages/CreatePage';
 
 function App() {
+  const [userCollection, setUserCollection] = useState([]); // Lifting state up!!! The card collection array doesn't live in CollectionPage.jsx, it lives here!!
+
+  function addToCollection() {
+    // To be implemented
+  }
 
   return (
     <BrowserRouter>
       <Routes>
         {/* The Layout wrapper is a wrapper around all of our page routes! */}
         <Route path='/' element={<Layout />}>
-          <Route index element={<CollectionPage />} />
+          <Route index element={<CollectionPage collection={userCollection} />} />
           <Route path='search' element={< SearchPage/>} />
           <Route path='card' element={<EasterEggPage />} />
-          <Route path='card/:id' element={<DetailedViewPage />} />
+          <Route path='card/:id' element={<DetailedViewPage collection={userCollection} onAdd={addToCollection} />} />
           <Route path='create' element={<CreatePage />} />
         </Route>
       </Routes>
--- src/pages/CollectionPage.jsx
+++ src/pages/CollectionPage.jsx
@@ -1,11 +1,10 @@
 import { useState } from 'react'
 
-const CollectionPage = () => {
-  const [cards, setCards] = useState([]); // Will need to change so that we check a JSON or something on mount
-
+const CollectionPage = ({ collection }) => {
+  
   return (
     <div>
-      {cards.length === 0 ? (
+      {collection.length === 0 ? (
         <p>There are no cards in your collection! Add your first card now :)</p>
       ) : (
         cards.map(card => <Card card={card} />)
