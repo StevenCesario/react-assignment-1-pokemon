58b70829a78c8603f151ec229fa743c92636cb76
StevenCesario <steven.cesario@proton.me>
2026-05-08 06:36:30 +0200

    detailed view page can now grab it from URL

--- src/pages/DetailedViewPage.jsx
+++ src/pages/DetailedViewPage.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const DetailedViewPage = () => {
+  const params = useParams(); 
+  const id = params.id; // Since we have `<Route path='card/:id' element={<DetailedViewPage />} />`
   return (
-    <div>DetailedViewPage</div>
+    <div>DetailedViewPage for card with ID: {id}</div>
   )
 }
 
