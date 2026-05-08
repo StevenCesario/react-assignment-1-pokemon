60414034e292ae5e51a4b631512c6972b8ed3c77
StevenCesario <steven.cesario@proton.me>
2026-05-08 15:14:42 +0200

    vibe coded CollectionItem styling

--- src/components/CollectionItem.jsx
+++ src/components/CollectionItem.jsx
@@ -1,18 +1,22 @@
 // import Card from "./Card" Not used anymore
 import { Link } from "react-router-dom"
 import CardImage from "./CardImage"
+import './CollectionItem.css';
 
 const CollectionItem = ({ card, onIncrease, onDecrease, onDelete }) => {
   return (
-    <div>
+    <div className="collection-item">
       {/* Now only includes the image! No Card component for the Name or Set! */}
       {/* Now also clickable! 🚀 */}
       <Link to={`/card/${card.id}`} state={{ cardData: card }} ><CardImage cardId={card.id} cardName={card.card_info.name} /></Link>
-      {/* The onClicks need to be arrow functions since we're passing arguments to keep them as function references! 🚀 */}
-      <button disabled={card.amount === 1} onClick={() => onDecrease(card.id)}>-</button>
-      <span>Amount: {card.amount}</span>
-      <button onClick={() => onIncrease(card.id)}>+</button>
-      <button onClick={() => onDelete(card.id)}>Remove from collection</button>
+
+      <div className="inventory-controls">
+        {/* The onClicks need to be arrow functions since we're passing arguments to keep them as function references! 🚀 */}
+        <button className="amount-btn" disabled={card.amount === 1} onClick={() => onDecrease(card.id)}>-</button>
+        <span className="amount-display">Amount: {card.amount}</span>
+        <button className="amount-btn" onClick={() => onIncrease(card.id)}>+</button>
+      </div>
+      <button className="remove-btn" onClick={() => onDelete(card.id)}>Remove from collection</button>
     </div>
   )
 }
